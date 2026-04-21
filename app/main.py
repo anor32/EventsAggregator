8c28fd9280d7cf151aed9d5309f9cf19ebc5531f
anor23 <burgchiken@gmail.com>
2026-04-21 13:40:17 +0300

    тест прекомита

--- app/main.py
+++ app/main.py
@@ -1,4 +1,11 @@
-from enpoints import  router
-from fastapi import FastAPI
+from .enpoints import  router
+from fastapi import (
+    FastAPI)
+import uvicorn
+import os
+import sys
+import json
+import  logging
+from typing   import   Dict,Any,Optional
 app = FastAPI()
 app.include_router(router)
