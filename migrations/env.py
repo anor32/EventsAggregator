91b86cf3ea20f4cd644c43f3bf3d0ed638a246f1
anor23 <burgchiken@gmail.com>
2026-04-25 18:24:00 +0300

    импорт моделей в env добавлен

--- migrations/env.py
+++ migrations/env.py
@@ -6,7 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 
 from app.settings.db_config import Base, get_sync_db_url
-
+from app.db.models import Place,Event
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
