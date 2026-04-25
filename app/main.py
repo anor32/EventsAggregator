fad92c5e7d98af5ca64ded2234b634af4c22ae1e
anor23 <burgchiken@gmail.com>
2026-04-26 00:52:19 +0300

    добавлен lifespan для фоновой задачи

--- app/main.py
+++ app/main.py
@@ -1,6 +1,27 @@
+import asyncio
+
 from fastapi import FastAPI
 
+from .clients.event_client import EventsProviderClient
+from .db.querries import DbRepository
 from .enpoints import router
+from .services import EventService
+from .settings.db_config import Session
+from .tests.synchronizers import BackgroundSynchronizer
 
-app = FastAPI()
+
+def lifespan(app: FastAPI):
+    session = Session()
+    db = DbRepository(session)
+    client = EventsProviderClient()
+    service = EventService(db, client)
+    synchronizer = BackgroundSynchronizer(service)
+    task = asyncio.create_task(synchronizer.synchronize())
+    yield
+    print("завершение работы приложения")
+
+    task.cancel()
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(router)
