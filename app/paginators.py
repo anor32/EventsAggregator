575e2304b88d351c0c4e7ae8a89f0a9865c2b26e
anor23 <burgchiken@gmail.com>
2026-04-26 19:57:10 +0300

    добавлен пагинатор для корректного составления пагинации страниц по базе данных

---
+++ app/paginators.py
@@ -0,0 +1,38 @@
+from urllib.parse import urlencode
+
+from fastapi import Request
+
+
+class ApiPaginator:
+    def __init__(
+        self,
+        count_objects: int,
+        path: str,
+        request: Request,
+        page=1,
+        page_size=20,
+    ):
+        self._base_url = str(request.base_url).rstrip("/")
+        self.page = page
+        self.page_size = page_size
+        self._next_url = ""
+        self.previous_url = ""
+        self.count_objects = count_objects
+        self.max_pages = (count_objects + page_size - 1) // page_size
+        self.path = path
+
+    def get_next_url(self) -> str | None:
+        if self.page >= self.max_pages:
+            return None
+        params = {"page": self.page + 1, "page_size": self.page_size}
+        query = urlencode(params)
+        self._next_url = f"{self._base_url}{self.path}?{query}"
+        return self._next_url
+
+    def get_previous_url(self) -> str | None:
+        if self.page - 1 < 1:
+            return None
+        params = {"page": self.page - 1, "page_size": self.page_size}
+        query = urlencode(params)
+        self.previous_url = f"{self._base_url}{self.path}?{query}"
+        return self.previous_url
