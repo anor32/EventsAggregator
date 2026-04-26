5cf0cc88a4d8b9f678f308e4946406a9e54bc4c4
anor23 <burgchiken@gmail.com>
2026-04-26 15:30:57 +0300

    перемещен модуль фоновой синхронизации из папки в тестов впапку приложения

--- app/main.py
+++ app/main.py
@@ -2,12 +2,13 @@ import asyncio
 
 from fastapi import FastAPI
 
+from app.synchronizers import BackgroundSynchronizer
+
 from .clients.event_client import EventsProviderClient
 from .db.querries import DbRepository
 from .enpoints import router
 from .services import EventService
 from .settings.db_config import Session
-from .tests.synchronizers import BackgroundSynchronizer
 
 
 def lifespan(app: FastAPI):
---
+++ app/synchronizers.py
@@ -0,0 +1,21 @@
+import asyncio
+
+from app.services import EventService
+
+
+class BackgroundSynchronizer:
+    def __init__(self, service: EventService):
+        self._service = service
+
+    async def synchronize(self, delay=24, date=None):
+        print("Начат процесс синхронизации")
+        if date is None:
+            date = "2000-01-01"
+
+        while True:
+            print(date)
+            print(f"Фоновавя синхронизация по дате {date}")
+            sync_dict = await self._service.sync_db(date)
+            date = sync_dict["last_changed_date"]
+            print(sync_dict)
+            await asyncio.sleep(delay * 60 * 60)
