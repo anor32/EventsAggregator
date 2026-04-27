9c8137b71ec61d304314634f978893ebfc8741f7
anor23 <burgchiken@gmail.com>
2026-04-27 21:16:41 +0300

    изменен url для запроса

--- app/settings/config.py
+++ app/settings/config.py
@@ -5,4 +5,4 @@ from dotenv import load_dotenv
 load_dotenv()
 EVENTS_API_KEY = os.getenv("EVENTS_API_KEY")
 # CLIENT_HOST = 'http://events-provider.dev-2.python-labs.ru'
-CLIENT_HOST = "http://student-system-events-provider-web.student-system-events-provider.svc"
+CLIENT_HOST = "http://student-system-events-provider-web.student-system-events-provider.svc:8000"
