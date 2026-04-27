8e36d94d3a35b2f1ca8107133b742516c445bd9a
anor23 <burgchiken@gmail.com>
2026-04-27 17:59:52 +0300

    задана константа для base урла клиента

--- app/settings/config.py
+++ app/settings/config.py
@@ -4,3 +4,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 EVENTS_API_KEY = os.getenv("EVENTS_API_KEY")
+# CLIENT_HOST = 'http://events-provider.dev-2.python-labs.ru'
+CLIENT_HOST = "http://student-system-events-provider-web.student-system-events-provider.svc"
