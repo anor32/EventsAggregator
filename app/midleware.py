ce9e13b95d92cfb946f56a5e6ee2101013e4da2a
anor23 <burgchiken@gmail.com>
2026-04-29 01:18:20 +0300

    создан файл  мидлеваре для отлова исключений в эндпоинте

---
+++ app/midleware.py
@@ -0,0 +1,29 @@
+from fastapi import Request
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.responses import JSONResponse
+
+from app.exceptions import ClientServerError, ObjectNotFound, WrongRequest
+from app.settings.logs_config import api_logger
+
+
+class ErrorHandlingMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        try:
+            response = await call_next(request)
+            return response
+        except ObjectNotFound as e:
+            api_logger.error(f"ошибка в эндпоинте{request.url} {e.message}")
+            return JSONResponse(status_code=404, content={"detail": e.message})
+        except (ValueError, WrongRequest) as e:
+            api_logger.error(f"ошибка в эндпоинте{request.url} {e.message}")
+            return JSONResponse(status_code=400, content={"detail": str(e)})
+        except ClientServerError as e:
+            api_logger.error(f"ошибка в эндпоинте{request.url} {e.message}")
+            return JSONResponse(
+                status_code=e.status_code, content={"detail": e.message}
+            )
+        except Exception as e:
+            api_logger.error(f"Ошибка сервера: {e}", exc_info=True)
+            return JSONResponse(
+                status_code=500, content={"detail": "Внутренняя ошибка"}
+            )
