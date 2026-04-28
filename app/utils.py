839baad7288ea0c15f9c369b0df95a03714a04c0
anor23 <burgchiken@gmail.com>
2026-04-29 01:20:06 +0300

    улучшена обработка ошибок применяются кастомные исключения

--- app/utils.py
+++ app/utils.py
@@ -1,32 +1,38 @@
 import asyncio
+from collections.abc import Callable
 
 from fastapi import HTTPException
+from httpx import AsyncClient, Request, Response
 
+from app.exceptions import ClientServerError, ObjectNotFound, WrongRequest
 from app.settings.logs_config import api_logger
 
 
-async def retry_request(client, request, max_retry=3, delay=1):
+async def retry_request(
+    client: AsyncClient, request: Request, max_retry=3, delay=1
+) -> Response:
     retry = 1
     response = await client.send(request)
-    while retry <= max_retry:
-        if response.status_code < 404:
-            return response
-        if response.status_code >= 404:
-            print(response.status_code)
-            retry += 1
-            await asyncio.sleep(delay)
+    while retry <= max_retry and response.status_code >= 500:
+        retry += 1
+        await asyncio.sleep(delay)
         response = await client.send(request)
-    if response.status_code >= 500:
-        raise ValueError("503|Ошибка внешнего сервера попробуйте позже")
-    if response.status_code == 404:
-        raise ValueError("404|Ошибка запрашиваемые данные не найдены")
-    if response.status_code == 400:
-        raise ValueError("400|Ошибка неправильный запрос клиента")
 
-    return response
+    if response.is_success:
+        return response
+    elif response.status_code >= 500:
+        raise ClientServerError(
+            "Ошибка внешнего сервера попробуйте позже", status_code=503
+        )
+    elif response.status_code == 404:
+        raise ObjectNotFound(
+            "Ошибка запрашиваемые данные от клиента не найдены "
+        )
+    elif response.status_code == 400:
+        raise WrongRequest("Ошибка неправильный запрос клиента", 400)
 
 
-def default_endpoint_exception(func):
+def default_endpoint_exception(func: Callable):
     async def wrapper():
         try:
             resp = await func()
