7e73c37372bddd0166c0ee58c4a08b8c1a72c754
anor23 <burgchiken@gmail.com>
2026-05-09 20:18:46 +0300

    исправлены относительные импорты в main

--- app/main.py
+++ app/main.py
@@ -1,30 +1,41 @@
 import asyncio
+import logging
 
 from fastapi import FastAPI
 
-from .api.endpoints import router
-from .api.middlewares import ErrorHandlingMiddleware
-from .clients.event_client import EventsProviderClient
-from .core.synchronizers import BackgroundSynchronizer
-from .db.querries import DbRepository
-from .services import EventService
-from .settings.db_config import Session
+from app.api.endpoints import router
+from app.api.middlewares import ErrorHandlingMiddleware
+from app.clients.event_client import EventsProviderClient
+from app.core.synchronizers import BackgroundSynchronizer
+from app.db.queries import DbRepository
+from app.services import EventService
+from app.settings.db_config import Session
+
+logger = logging.getLogger(__name__)
 
 
 def lifespan(app: FastAPI):
-    session = Session()
+    session = Session()  # <-- исправлено Session()
     db = DbRepository(session)
     client = EventsProviderClient()
     service = EventService(db, client)
     synchronizer = BackgroundSynchronizer(service)
+
     task = asyncio.create_task(synchronizer.synchronize())
+
     yield
-    print("завершение работы приложения")
+
+    logger.info("Завершение работы приложения")
 
     task.cancel()
+    try:
+        asyncio.get_running_loop().run_until_complete(task)
+    except asyncio.CancelledError:
+        pass
+    finally:
+        session.close()
 
 
 app = FastAPI(lifespan=lifespan)
 app.add_middleware(ErrorHandlingMiddleware)
-
 app.include_router(router)
